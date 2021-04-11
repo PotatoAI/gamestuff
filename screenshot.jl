719e71a48443d7bfe90b77b77009d94b724d6610
Max Gonzih <gonzih@gmail.com>
2021-04-10 21:59:35 -0400

    Huh

--- screenshot.jl
+++ screenshot.jl
@@ -3,6 +3,7 @@ module Screenshot
 export ScreenshotWindow
 
 function ScreenshotWindow(winname::String, filename::String)
+    ENV["DISPLAY"] = ":0"
     run(`magick.exe import -window $winname $filename`)
 end
 
